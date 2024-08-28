8bbec52c1b58a921cdb1752c33b702b95b5c5e61
AdamPar <adamparkitny7@gmail.com>
2024-08-28 20:22:34 +0200

    feat: remove oled tests, access point setup

--- Smart-Cube-Tester.ino
+++ Smart-Cube-Tester.ino
@@ -1,61 +1,57 @@
-
+// ====== INCLUDES =======
 #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SH110X.h>
+#include <ESP8266WiFi.h>
 
-#define i2c_Address 0x3c // 0x3d
+// ====== DEFINES =======
+#define i2c_Address 0x3c
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET -1
 
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
-#define OLED_RESET -1   //   QT-PY / XIAO
+// ====== OBJECTS =======
 Adafruit_SH1106G display = Adafruit_SH1106G(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
+// ===== VARIABLES ======
+const char *ssid_ap = "Smart-Cube-Tester";
+//const char* password_ap = "secret";
+
+// ======= SETUP ========
 void setup()   {
 
   Serial.begin(9600);
-
-  // Show image buffer on the display hardware.
-  // Since the buffer is intialized with an Adafruit splashscreen
-  // internally, this will display the splashscreen.
+  
+  // set esp8266 as access point
+  WiFi.softAP(ssid_ap);
+  Serial.println("Smart-Cube-Tester ssid: " + String(ssid_ap));
+  Serial.print("Smart-Cube-Tester IP address: ");
+  Serial.println(WiFi.softAPIP());
 
   delay(250); // wait for the OLED to power up
-  display.begin(i2c_Address, true); // Address 0x3C default
- //display.setContrast (0); // dim display
- 
-  display.display();
-  delay(2000);
-
-  // Clear the buffer.
+  display.begin(i2c_Address, true);
+  delay(250);
   display.clearDisplay();
 
-  // draw a single pixel
-  display.drawPixel(10, 10, SH110X_WHITE);
-  // Show the display buffer on the hardware.
-  // NOTE: You _must_ call display after making any drawing commands
-  // to make them visible on the display hardware!
-  display.display();
+  
+}
+
+// ======== LOOP ========
+void loop() {
+  mockOutputOLED();
   delay(2000);
-  display.clearDisplay();
+}
 
+// ====== FUNCTIONS ======
 
-  // text display tests
-  display.setTextSize(1);
-  display.setTextColor(SH110X_WHITE);
-  display.setCursor(0, 0);
-  display.println("test OLED screen");
-  display.setTextColor(SH110X_BLACK, SH110X_WHITE); // 'inverted' text
-  display.println(3.141592);
+void mockOutputOLED(){
+
   display.setTextSize(2);
   display.setTextColor(SH110X_WHITE);
-  display.print("0x"); display.println(0xDEADBEEF, HEX);
+  display.setCursor(10, 10);
+  display.println("Command:");
+  display.setCursor(10, 30);
+  display.println("undefined");
   display.display();
-  delay(2000);
-  //display.clearDisplay();
 }
-
-
-void loop() {
-
-}
-
