1afddd0c1ef5349cda3079db2615e10f6571cfc6
aparkitny <adamparkitny7@gmail.com>
2024-09-01 13:18:06 +0200

    feat: add command recieve

--- Smart-Cube-Tester.ino
+++ Smart-Cube-Tester.ino
@@ -3,7 +3,12 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SH110X.h>
+
 #include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <ESP8266WebServer.h>
+
+#include <ArduinoJson.h>
 
 // ====== DEFINES =======
 #define i2c_Address 0x3c
@@ -13,10 +18,62 @@
 
 // ====== OBJECTS =======
 Adafruit_SH1106G display = Adafruit_SH1106G(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+ESP8266WebServer server(80);
 
 // ===== VARIABLES ======
 const char *ssid_ap = "Smart-Cube-Tester";
 //const char* password_ap = "secret";
+
+// ====== FUNCTIONS ======
+
+void mockOutputOLED(){
+
+  display.setTextSize(2);
+  display.setTextColor(SH110X_WHITE);
+  display.setCursor(10, 10);
+  display.println("Command:");
+  display.setCursor(10, 30);
+  display.println("undefined");
+  display.display();
+}
+
+void handleHomePage() {
+    server.send(200, "text/plain", "Smart Cube Tester");
+}
+
+void handleOnNotFound(){
+  server.send(404, "text/plain", "Not found");
+}
+
+void handleCommand() {
+  if (server.hasArg("plain")) {
+    String json = server.arg("plain");
+    Serial.println("Received JSON: " + json);
+
+    StaticJsonDocument<200> doc;
+    DeserializationError error = deserializeJson(doc, json);
+
+    if (error) {
+      Serial.println("Failed to parse JSON");
+      server.send(400, "application/json", "{\"status\":\"error\",\"message\":\"Invalid JSON\"}");
+      return;
+    }
+
+    String command = doc["command"];
+    Serial.println("Command: " + command);
+
+    // // Process the command
+    // if (command == "LED_ON") {
+    //   digitalWrite(LED_BUILTIN, LOW); // Turn on LED
+    // } else if (command == "LED_OFF") {
+    //   digitalWrite(LED_BUILTIN, HIGH); // Turn off LED
+    // }
+
+    server.send(200, "application/json", "{\"status\":\"success\",\"message\":\"Command received\"}");
+  } else {
+    server.send(400, "application/json", "{\"status\":\"error\",\"message\":\"No JSON received\"}");
+  }
+}
 
 // ======= SETUP ========
 void setup()   {
@@ -34,24 +91,19 @@ void setup()   {
   delay(250);
   display.clearDisplay();
 
+  server.on("/", handleHomePage);
+  server.on("/command", HTTP_POST, handleCommand); 
+  server.onNotFound(handleOnNotFound);
+  server.begin();
   
 }
 
 // ======== LOOP ========
 void loop() {
+  server.handleClient();
+
   mockOutputOLED();
-  delay(2000);
+  delay(1000);
+  
 }
 
-// ====== FUNCTIONS ======
-
-void mockOutputOLED(){
-
-  display.setTextSize(2);
-  display.setTextColor(SH110X_WHITE);
-  display.setCursor(10, 10);
-  display.println("Command:");
-  display.setCursor(10, 30);
-  display.println("undefined");
-  display.display();
-}
